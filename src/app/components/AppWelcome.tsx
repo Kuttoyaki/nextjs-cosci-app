217c116249f158be7d2f63d8ecdcd3c7a7be6c9b
Wongsapat <conor63d@gmail.com>
2025-05-27 15:51:47 +0700

    Create Page

--- src/app/components/AppWelcome.tsx
+++ src/app/components/AppWelcome.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
@@ -6,29 +6,36 @@ import { useState } from "react";
 type AppWelcomeProps = {
     headTitle: string;
     isShow: boolean;
-}
+};
 
-export default function AppWelcome({headTitle, isShow}: AppWelcomeProps) {
-    const [title, seTitle] = useState("Welcome to my Page");
-    const year = <p>2025</p>;
+export default function AppWelcome({ headTitle, isShow }: AppWelcomeProps) {
+    const [title, setTitle] = useState("Welcome to my Page");
+    const year = 2025;
+
     const handleClick = () => {
-        seTitle("Welcome to SWU")
-        alert("DONKI DONKI");
-    }
+    setTitle("Welcome to SWU");
+    alert("DONKI DONKI");
+    };
 
     return (
-        <>
-            <h1>{headTitle}</h1>
-            <p>{title.toUpperCase()}</p>
-            <Button onClick={handleClick}>Click Me</Button>
-            <button className="bg-gray-900 p-3 m-3 text-purple-600 rounded-lg" onClick={handleClick}>Click here!</button>
-            {year}     
-            { 
-                isShow && <p>Date: 27/05/2025</p>
-            } 
-            {
-                isShow ? <p>Hello Kutto</p> : <p>Hello Kuttoyaki</p>
-            }
-        </>
+    <>
+        <h1>{headTitle}</h1>
+        <p>{process.env.NEXT_PUBLIC_APP_NAME ?? "COSCI Online Shop"}</p>
+        <p>{title.toUpperCase()}</p>
+
+        <Button onClick={handleClick}>Click Me</Button>
+        <button
+        className="bg-gray-900 p-3 m-3 text-purple-600 rounded-lg"
+        onClick={handleClick}
+        >
+        Click here!
+        </button>
+
+        <p>{year}</p>
+
+        {isShow && <p>Date: 27/05/2025</p>}
+
+        {isShow ? <p>Hello Kutto</p> : <p>Hello Kuttoyaki</p>}
+    </>
     );
 }
---
+++ src/app/product/page.tsx
@@ -0,0 +1,7 @@
+export default function Product(){
+    return (
+        <>
+            <p>List of product</p>
+            </>
+    );
+}
