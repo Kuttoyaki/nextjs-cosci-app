816d5a6cf015a050a6a39f8cd3d7cf05b6c5ab3d
Wongsapat <conor63d@gmail.com>
2025-05-27 11:18:44 +0700

    Create Component

--- src/app/components/AppWelcome.tsx
+++ src/app/components/AppWelcome.tsx
@@ -1,7 +1,18 @@
 export default function AppWelcome() {
+    const title = "Welcome to my Page";
+    const year = <p>2025</p>;
+    const isShow = false;
+
     return (
         <>
-            <p>Welcome to my Page</p>        
+            <p>{title.toUpperCase()}</p>  
+            {year}     
+            { 
+                isShow && <p>Date: 27/05/2025</p>
+            } 
+            {
+                isShow ? <p>Hello Kutto</p> : <p>Hello Kuttoyaki</p>
+            }
         </>
     );
 }
