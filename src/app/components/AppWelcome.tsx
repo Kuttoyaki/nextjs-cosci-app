c301f13ae47f33c5ce28b645467b1d5c56b9de1a
Wongsapat <conor63d@gmail.com>
2025-05-27 11:36:53 +0700

    Create Button

--- src/app/components/AppWelcome.tsx
+++ src/app/components/AppWelcome.tsx
@@ -1,11 +1,15 @@
 export default function AppWelcome() {
     const title = "Welcome to my Page";
     const year = <p>2025</p>;
-    const isShow = false;
+    const isShow = true;
+    const handleClick = () => {
+        alert("Hello TypeScript");
+    }
 
     return (
         <>
-            <p>{title.toUpperCase()}</p>  
+            <p>{title.toUpperCase()}</p>
+            <button className="bg-yellow-100 p-3 m-3 text-orange-400 rounded-lg" onClick={handleClick}>Click here!</button>  
             {year}     
             { 
                 isShow && <p>Date: 27/05/2025</p>
