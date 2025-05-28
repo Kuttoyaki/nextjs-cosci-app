40ba5224157d925dc989e44305b39715ab110f9f
Wongsapat <conor63d@gmail.com>
2025-05-28 10:39:59 +0700

    Adjust Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import Features01Page from "@/components/features-01/features-01";
+import Footer01Page from "@/components/footer-02/footer-02";
 import Hero02 from "@/components/hero-02/hero-02";
 
 export default function Home() {
   return (
     <>
       <Hero02 />
+
+      <Features01Page />
+      
+      <Footer01Page />
     </>
   );
 }
