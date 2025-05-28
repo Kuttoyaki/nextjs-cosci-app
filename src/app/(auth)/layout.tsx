0ed6abc4c8e74f4ee03ca3f0a7e7bececaf56404
Wongsapat <conor63d@gmail.com>
2025-05-28 11:20:10 +0700

    Create Layout Log in

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,32 +1,31 @@
 import type { Metadata } from "next";
 import { Orbitron } from "next/font/google";
 import "../globals.css";
-import Navbar01Page from "@/components/navbar-01/navbar-01";
 
 const font = Orbitron({
-  subsets: ["latin"],
-  weight: ["400","500","600","700","800","900"],
+    subsets: ["latin"],
+    weight: ["400","500","600","700","800","900"],
 });
 
 export const metadata: Metadata = {
-  title: "COSCI Merch",
-  description: "Lastest product 2025",
+    title: "Log in",
+    description: "Lastest product 2025",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
+    return (
     <html lang="en">
-      <body
+        <body
         className= {`${font.className}`}
-      >
-        <Navbar01Page />
+        >
+        
 
         {children}
-      </body>
+        </body>
     </html>
-  );
+    );
 }
